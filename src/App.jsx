8fbe6140dcc5ec3021057492f7b535ec773f8e26
egemenunall <148786767+egemenunall@users.noreply.github.com>
2024-09-01 19:31:52 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,10 @@ export default function App() {
   return (
     <div>
       <Routes>
-        <Route path='/client/' element={<Homepage products={data?data:""} />}></Route>
-        <Route path='/client/product/:id' element={<ProductContentPage  products={data?data:""}/>}></Route>
-        <Route path="/client/about" element={<AboutPage/>}></Route>
-        <Route path="/client/products" element={<ProductAllPage products={data?data:""} />}></Route>
+        <Route path='/' element={<Homepage products={data?data:""} />}></Route>
+        <Route path='/product/:id' element={<ProductContentPage  products={data?data:""}/>}></Route>
+        <Route path="/about" element={<AboutPage/>}></Route>
+        <Route path="/products" element={<ProductAllPage products={data?data:""} />}></Route>
       </Routes>
     </div>
   
