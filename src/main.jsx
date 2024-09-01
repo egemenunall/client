63813abe1d8cb689b820062c497f95ccdab34413
egemenunall <148786767+egemenunall@users.noreply.github.com>
2024-09-01 19:44:58 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css';
 import "./App.css";
-import {HashRouter} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 
 
 const basename = '/client/';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
